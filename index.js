b242e11db0a79a3e44a8963c309c97ca7b335456
git-austinmccollom <austinmccollom@gmail.com>
2020-09-28 17:22:26 -0500

    post endpoint with validation

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 //initialize
-const express = require('express');
+const shortid = require("shortid");
+const express = require("express");
 const server = express();
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json({ hello: 'world' })
-})
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "world" });
+});
 
 // data
+const users = [];
 
-const users = [
+//endpoints
 
-]
+// - POST
+server.post("/api/users", (req, res) => {
+  const data = req.body;
+  if (data.name && data.bio) {
+    const thisId = shortid.generate();
+    users.push({ id: thisId, ...data });
+    res.status(201).json({ data, users });
+  } else {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+  }
+});
 
-// endpoints
+// - GET
 
+// - GET by id
 
+// - DELETE
+
+// - PUT
 
 //footer
-
 const port = 5000;
-server.listen(port, () => console.log('server running...'));
+server.listen(port, () => console.log("server running..."));
